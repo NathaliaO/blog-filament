13979ee24f38e802075cc75a35222ef334ca7feb
Nathalia Oliveira <nathaliaoliveiramc@gmail.com>
2024-04-03 16:42:42 -0300

    Profile implemented

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -27,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile(EditProfile::class)
             ->favicon('https://rockbuzz.com.br/images/site/favicon.png')
             ->colors([
                 'primary' => Color::Amber,
